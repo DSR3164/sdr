ec695b85e9343f6673a5ceb287c1a0c3f9cf642d
Excalibur <104138693+DSR3164@users.noreply.github.com>
2025-12-03 17:21:31 +0700

    refactor: cleanup gardner.py

--- pluto/dev/gardner.py
+++ pluto/dev/gardner.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def gardner(complex_symbols_after_convolve):
-    SPS = 10 # Samples per symbol
+def gardner(complex_symbols_after_convolve, SPS = 10):
     K1 = 0; K2 = 0; p1 = 0; p2 = 0; e = 0; offset = 0; Kp = 4
     BnTs = 0.01; 
     zeta = np.sqrt(2)/2
@@ -10,9 +9,6 @@ def gardner(complex_symbols_after_convolve):
     K1 = (-4*zeta*teta)/((1 + 2*zeta*teta + teta**2)*Kp)
     K2 = (-4*teta**2)/((1 + 2*zeta*teta + teta**2)*Kp)
     s = complex_symbols_after_convolve
-    plt.plot(s)
-    plt.show()
-    s = s[169250:171800]
     offset_list = []
     err_list = []
     mean_err = []
@@ -28,36 +24,35 @@ def gardner(complex_symbols_after_convolve):
         offset_list.append(offset)
         err_list.append(e)
     
-    for i in range(4, 15):
-        temp = []
-        for x in range(0, len(s) - SPS*2-i, 10):
-            of = x + i
-            e  = (np.real(s[of + SPS + SPS]) - np.real(s[of + SPS])) * np.real(s[of + SPS//2 + SPS])
-            e += (np.imag(s[of + SPS + SPS]) - np.imag(s[of + SPS])) * np.imag(s[of + SPS//2 + SPS])
-            temp.append(e)
-        mean_err.append(np.mean(temp))
 
-    return offset, offset_list, err_list, mean_err
+    right = 0
+    for y in range(0, 2):
+        for i in range(right - SPS//2, right + SPS//2+1):
+            temp = []
+            for x in range(0, len(s) - SPS*2-i, SPS):
+                of = x + i
+                e  = (np.real(s[of + SPS + SPS]) - np.real(s[of + SPS])) * np.real(s[of + SPS//2 + SPS])
+                e += (np.imag(s[of + SPS + SPS]) - np.imag(s[of + SPS])) * np.imag(s[of + SPS//2 + SPS])
+                temp.append(e)
+            mean_err.append(np.mean(temp))
+        right = int(np.round(np.argmin(np.abs(mean_err))))
+        if y != 1:
+            mean_err.clear()
+
+    offset_list = np.array(offset_list, dtype=int)
+    err_list = np.array(err_list, dtype=float)
+    mean_err = np.array(mean_err, dtype=float)
+
+    return right, offset_list, err_list, mean_err
 
 rx = np.fromfile(f"/home/plutoSDR/sdr/pluto/dev/sdr_to_sdr.pcm", dtype=np.int16)
-samples_rx = []
+SPS = 10
+samples_rx = (rx[0::2] + 1j * rx[1::2]) / np.max(rx)
+samples_mf = np.convolve(samples_rx, np.ones(SPS))
 
-for x in range(0, len(rx), 2):
-    samples_rx.append((rx[x]+ 1j * rx[x+1])/np.max(rx))
-
-a = np.ones(10)
-time = np.arange(len(samples_rx))
-pila = np.convolve(samples_rx, a)
-
-offset, offset_list, err_list, mean_err = gardner(pila)
-signal = []
-for x in range(0, len(offset_list)-1):
-    signal.append(offset_list[x] + 10*x)
-
-signal = np.array(signal)
-signal = pila[signal]
-
-print(offset)
+right, offset_list, err_list, mean_err = gardner(samples_mf, SPS)
+offset_mask = np.array([v + SPS*i for i, v in enumerate(offset_list)])
+signal = samples_mf[offset_mask]
 
 plt.figure()
 plt.subplot(2,1,1)
@@ -69,10 +64,10 @@ plt.axvline()
 
 plt.subplot(2,1,2)
 plt.title("Сигнал после согласованного фильтра")
-# plt.xlim([5, 192])
-plt.ylim([-3, 3])
-plt.plot(np.arange(len(np.real(signal))), np.real(signal)/10)
-plt.plot(np.arange(len(np.imag(signal))), np.imag(signal)/10)
+plt.xlim([0, len(signal)])
+plt.ylim([-max(np.real(signal))*3/SPS, max(np.real(signal))*3/SPS])
+plt.plot(np.arange(len(np.real(signal))), np.real(signal)/SPS)
+plt.plot(np.arange(len(np.imag(signal))), np.imag(signal)/SPS)
 plt.grid()
 
 plt.figure()
@@ -80,8 +75,8 @@ plt.title("Средняя ошибка к сдвигу от правильног
 plt.axhline()
 plt.axvline()
 plt.grid()
-plt.scatter(np.arange(-5,6), mean_err)
-plt.plot(np.arange(-5,6), mean_err)
+plt.scatter(np.arange(-SPS//2, SPS//2 + 1), mean_err)
+plt.plot(np.arange(-SPS//2, SPS//2 + 1), mean_err)
 
 plt.figure()
 plt.title("Ошибка и сдвиг к номеру символа")
