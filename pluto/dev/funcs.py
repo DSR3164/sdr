410a6dcc4571326ae2913b2b0b4291dc7a87fbb0
Excalibur <104138693+DSR3164@users.noreply.github.com>
2025-12-04 14:43:37 +0700

    refactor: move out funcs

--- pluto/dev/funcs.py
+++ pluto/dev/funcs.py
@@ -24,7 +24,6 @@ def gardner(complex_symbols_after_convolve, SPS = 10):
         offset_list.append(offset)
         err_list.append(e)
     
-
     right = 0
     for y in range(0, 2):
         for i in range(right - SPS//2, right + SPS//2+1):
@@ -35,6 +34,7 @@ def gardner(complex_symbols_after_convolve, SPS = 10):
                 e += (np.imag(s[of + SPS + SPS]) - np.imag(s[of + SPS])) * np.imag(s[of + SPS//2 + SPS])
                 temp.append(e)
             mean_err.append(np.mean(temp))
+            temp.clear()
         right = int(np.round(np.argmin(np.abs(mean_err))))
         if y != 1:
             mean_err.clear()
@@ -44,45 +44,3 @@ def gardner(complex_symbols_after_convolve, SPS = 10):
     mean_err = np.array(mean_err, dtype=float)
 
     return right, offset_list, err_list, mean_err
-
-rx = np.fromfile(f"/home/plutoSDR/sdr/pluto/dev/sdr_to_sdr.pcm", dtype=np.int16)
-SPS = 10
-samples_rx = (rx[0::2] + 1j * rx[1::2]) / np.max(rx)
-samples_mf = np.convolve(samples_rx, np.ones(SPS))
-
-right, offset_list, err_list, mean_err = gardner(samples_mf, SPS)
-offset_mask = np.array([v + SPS*i for i, v in enumerate(offset_list)])
-signal = samples_mf[offset_mask]
-
-plt.figure()
-plt.subplot(2,1,1)
-plt.title("Сигнальное созвездие")
-plt.scatter(np.real(signal), np.imag(signal))
-plt.axhline()
-plt.grid(True)
-plt.axvline()
-
-plt.subplot(2,1,2)
-plt.title("Сигнал после согласованного фильтра")
-plt.xlim([0, len(signal)])
-plt.ylim([-max(np.real(signal))*3/SPS, max(np.real(signal))*3/SPS])
-plt.plot(np.arange(len(np.real(signal))), np.real(signal)/SPS)
-plt.plot(np.arange(len(np.imag(signal))), np.imag(signal)/SPS)
-plt.grid()
-
-plt.figure()
-plt.title("Средняя ошибка к сдвигу от правильного сдвига")
-plt.axhline()
-plt.axvline()
-plt.grid()
-plt.scatter(np.arange(-SPS//2, SPS//2 + 1), mean_err)
-plt.plot(np.arange(-SPS//2, SPS//2 + 1), mean_err)
-
-plt.figure()
-plt.title("Ошибка и сдвиг к номеру символа")
-plt.grid()
-plt.plot(err_list, label='Error')
-plt.plot(offset_list, label='Offset')
-plt.legend()
-
-plt.show()
---
+++ pluto/dev/main.py
@@ -0,0 +1,43 @@
+from funcs import *
+
+rx = np.fromfile(f"/home/plutoSDR/sdr/pluto/dev/sdr_to_sdr.pcm", dtype=np.int16)
+SPS = 10
+samples_rx = (rx[0::2] + 1j * rx[1::2]) / np.max(rx)
+samples_mf = np.convolve(samples_rx, np.ones(SPS))
+
+right, offset_list, err_list, mean_err = gardner(samples_mf, SPS)
+offset_mask = np.array([v + SPS*i for i, v in enumerate(offset_list)])
+signal = samples_mf[offset_mask]
+
+plt.figure()
+plt.subplot(2,1,1)
+plt.title("Сигнальное созвездие")
+plt.scatter(np.real(signal), np.imag(signal))
+plt.axhline()
+plt.grid(True)
+plt.axvline()
+
+plt.subplot(2,1,2)
+plt.title("Сигнал после согласованного фильтра")
+plt.xlim([0, len(signal)])
+plt.ylim([-max(np.real(signal))*3/SPS, max(np.real(signal))*3/SPS])
+plt.plot(np.arange(len(np.real(signal))), np.real(signal)/SPS)
+plt.plot(np.arange(len(np.imag(signal))), np.imag(signal)/SPS)
+plt.grid()
+
+plt.figure()
+plt.title("Средняя ошибка к сдвигу от правильного сдвига")
+plt.axhline()
+plt.axvline()
+plt.grid()
+plt.scatter(np.arange(-SPS//2, SPS//2 + 1), mean_err)
+plt.plot(np.arange(-SPS//2, SPS//2 + 1), mean_err)
+
+plt.figure()
+plt.title("Ошибка и сдвиг к номеру символа")
+plt.grid()
+plt.plot(err_list, label='Error')
+plt.plot(offset_list, label='Offset')
+plt.legend()
+
+plt.show()
