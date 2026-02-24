e35458aa30eb118a5cbe8581b06fc8cf1b2cbb80
Excalibur <104138693+DSR3164@users.noreply.github.com>
2026-02-24 21:22:01 +0700

    feat: write ofdm to file for debug

--- pluto/dev/main.py
+++ pluto/dev/main.py
@@ -2,14 +2,34 @@ from funcs import *
 
 app.use_app("pyqt5")
 
-rx = np.fromfile("/home/plutoSDR/sdr/pluto/dev/rx.pcm", dtype=np.int16)
+rx = np.fromfile("pluto/dev/pcm/ofdm2.pcm", dtype=np.int16)
 SPS = 10
 bg = 'black'
-samples_rx = (rx[0::2] + 1j * rx[1::2])
+samples_rx = rx[0::2] + 1j * rx[1::2]
+ofdm_symbols_len = 128
+cp_len = 32
+symbol_len = cp_len + ofdm_symbols_len
+symbols_count = len(samples_rx) // (ofdm_symbols_len + cp_len)
 print(len(rx))
-samples_mf = np.convolve(samples_rx, np.ones(SPS))
-signal, offset_list = gardner(samples_mf, SPS)
-samples_rx = costas_loop(signal)
-vispy_line(samples_rx, bgcolor=bg)
-vispy_constelation(samples_rx, bgcolor=bg)
+print("symbols:", symbols_count)
+
+ofdm_symbols = samples_rx.reshape(symbols_count, ofdm_symbols_len + cp_len)
+ofdm_symbols_without_cp = []
+
+for x in ofdm_symbols:
+    ofdm_symbols_without_cp.append(x[cp_len:])
+    print(len(x[cp_len:]))
+
+ofdm_symbols_without_cp = np.array(ofdm_symbols_without_cp).reshape(symbols_count, ofdm_symbols_len)
+ofdm_symbols_after_fft = []
+print(ofdm_symbols_without_cp.shape)
+
+
+for x in ofdm_symbols_without_cp:
+    ofdm_symbols_after_fft.append(np.fft.fft(x))
+ofdm_symbols_after_fft = np.array(ofdm_symbols_after_fft).reshape(symbols_count * ofdm_symbols_len, 1)
+
+vispy_line(ofdm_symbols_after_fft, bgcolor=bg)
+vispy_constelation(ofdm_symbols_after_fft, bgcolor=bg)
+
 app.run()
--- pluto/dev/src/main.cpp
+++ pluto/dev/src/main.cpp
@@ -1,75 +1,22 @@
 #include "pluto_lib.h"
 #include <iostream>
+#include "fstream"
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    (void)argc;
-    double shift = 0e3;
-    double carrier = 5.37e8;
-    sdr_config_t sdr1(
-        argv[1], 1920,
-        // tx, rx
-        1e6,
-        carrier + shift, carrier + shift,
-        -10.0, 45.0);
-
-    if (init(&sdr1) != 0)
-    {
-        printf("Initialization error\n");
-        return -1;
-    }
-
-    int N = 192000;
+    int N = 128 * 10 * 2;
     vector<int> bits(N);
     vector<int16_t> tx_buffer;
-    vector<int16_t> rx_buffer(1920 * 2);
 
-    // file_to_bits("", bits);
     gen_bits(N, bits);
-    qpsk_3gpp(bits, tx_buffer, true);
+    ofdm(bits, tx_buffer, 128, 32, 125);
 
-    // implement_barker(tx_buffer);
+    std::fstream file("../pcm/ofdm2.pcm", std::ios::out | std::ios::binary);
 
-    void *tx_buffs[] = {tx_buffer.data()};
-    void *rx_buffs[] = {rx_buffer.data()};
-    FILE *file_rx;
-    file_rx = fopen("../pcm/rxtest.pcm", "wb");
-    if (strcmp(argv[2], "rx") == 0)
-        file_rx = fopen("../pcm/rx.pcm", "wb");
-    FILE *file_tx = fopen("../pcm/tx1.pcm", "wb");
-
-    int flags = SOAPY_SDR_HAS_TIME;
-    long long timeNs;
-    long timeoutUs = 400000;
-    int sr = 0;
-    int st = 0;
-    int count = 0;
-    float buff_count = (tx_buffer.size() / (1920 * 2));
-    cout << "Buffs: " << buff_count << endl;
-    fwrite(tx_buffs[0], 2 * (int)buff_count*1920 * sizeof(int16_t), 1, file_tx);
-
-    for (int k = 0; k < 15000000; k++)
-    {
-        tx_buffs[0] = static_cast<void *>(tx_buffer.data() + 1920 * 2 * (k < buff_count ? k : 0));
-        sr = sdr1.sdr->readStream(sdr1.rxStream, rx_buffs, sdr1.buffer_size, flags, timeNs, timeoutUs);
-        long long tx_time = timeNs + (4 * 1000 * 1000); // Schedule TX 4ms ahead
-        if (strcmp(argv[2], "rx") != 0)
-        {
-            st = sdr1.sdr->writeStream(sdr1.txStream, (const void *const *)tx_buffs, sdr1.buffer_size, flags, tx_time, timeoutUs);
-            (void)st;
-        }
-        fwrite(rx_buffs[0], 2 * sr * sizeof(int16_t), 1, file_rx);
-        fwrite(tx_buffs[0], 2 * sdr1.buffer_size * sizeof(int16_t), 1, file_tx);
-        if (k % 520 == 0 && k != 0)
-        {
-            count += 1;
-            cout << count << " second\t" << k << " buffs" << endl;
-        }
-    }
-    fclose(file_rx);
-    fclose(file_tx);
-    deinit(&sdr1);
+    file.write(reinterpret_cast<char *>(tx_buffer.data()),
+               tx_buffer.size() * sizeof(int16_t));
+    file.close();
     return 0;
 }
